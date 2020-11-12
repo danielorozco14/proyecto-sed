d4046c772228c5df9b4957d73c5c9d997bfd0efb
danielorozco14 <00200617@uca.edu.sv>
2020-11-11 20:01:02 -0600

    Cambiando package json

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
+const { Mongoose } = require("mongoose");
 
 /*
   Base de datos
