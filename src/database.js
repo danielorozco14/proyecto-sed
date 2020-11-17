dbddfaa78439fa8b13bf1e843016621be3571ea9
danielorozco14 <00200617@uca.edu.sv>
2020-11-16 18:13:04 -0600

    Agregada configuracion para autenticacion en BD

--- src/database.js
+++ src/database.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 
+const USUARIO_DB = "aeropuertoUser";
+const PWD_DB = "Mlo9$rGH";
+const IP_DB = "127.0.0.1:27017";
+
+
 mongoose
-  .connect("mongodb://localhost/aeropuerto", {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  })
+  .connect(
+    `mongodb://${USUARIO_DB}:${PWD_DB}@${IP_DB}/aeropuerto?authSource=aeropuerto`,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("Conectado a MongoDB"))
   .catch((error) => console.error(error));
